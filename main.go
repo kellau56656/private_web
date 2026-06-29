f8b0c5693e869f1c3de94605125be7758f2a30d6
kellau56656 <kelauzxc@gmail.com>
2026-06-29 15:56:03 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -11,65 +11,61 @@ import (
 )
 
 type File struct {
-	ID       string
 	Filename string
 	URL      string
-	Type     string
 }
 
-var id string
-var out *os.File
-var file multipart.File
-var header *multipart.FileHeader
-var url string
+var files = make(map[string]File)
 
-func Service(file multipart.File, header *multipart.FileHeader) (string, error) {
-	id = uuid.New().String()
-	Filename := id + filepath.Ext(header.Filename)
-	path := "upload/" + Filename
-	os.Mkdir("upload", 0755)
+func Service(file multipart.File, header *multipart.FileHeader) (File, error) {
+	id := uuid.New().String()
+	filename := id + filepath.Ext(header.Filename)
+	path := "upload/" + filename
+	_ = os.Mkdir("upload", 0755)
 	out, err := os.Create(path)
 	if err != nil {
-		return "", err
+		return File{}, err
 	}
 	defer out.Close()
 	_, err = io.Copy(out, file)
 	if err != nil {
-		return "", err
+		return File{}, err
 	}
-	url := "http://localhost:8080/upload"
-	return url, nil
+	f := File{
+		Filename: filename,
+		URL:      "/upload/" + filename,
+	}
+	return f, nil
 }
 func Upload(c *gin.Context) {
 	file, header, err := c.Request.FormFile("file")
-
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	defer file.Close()
-	url, err := Service(file, header)
+
+	f, err := Service(file, header)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
+
 	c.JSON(200, gin.H{
-		"url": url,
+		"file": f,
 	})
 }
+
 func main() {
 	r := gin.Default()
 
-	r.POST("/upload", Upload, func(c *gin.Context) {
-		c.JSON(200, gin.H{"file go to up": true})
-	})
+	r.POST("/upload", Upload)
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"ok": true})
-	})
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	r.Static("/upload", "./upload")
 	r.Run(":" + port)
 }
